825f3551f03726dbcd71950992ea5cd4e1c6c0d0
pranayb-5 <pranay.bhatia@arconnet.com>
2025-10-31 16:30:09 +0530

    Create arconazplugin.py

---
+++ arconazplugin.py
@@ -0,0 +1,28 @@
+import sys
+from ArconSDK import ArconSDK
+
+
+ 
+def get_sec(token,username,alias_name):
+    try:
+        arcon_sdk = ArconSDK(token)
+        data = [{
+        "UserName": username,
+        "AliasName": alias_name ,
+         "OpenForHours": 100  }]
+        response = arcon_sdk.get_credential(data)
+        if response:
+            print("response:", response)
+            return response
+        else:
+            print("Failed to obtain response")
+ 
+    except Exception as ex:
+        print("Exception occurred:", ex)
+ 
+ 
+if __name__ == "__main__":
+    token = sys.argv[1]
+    username = sys.argv[2]
+    alias_name = sys.argv[3]
+    sec=get_sec(token,username,alias_name)
