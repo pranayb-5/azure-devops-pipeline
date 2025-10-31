b4d22c0e543f6e1fdb9d8a26d6b48aa050582be5
pranayb-5 <pranay.bhatia@arconnet.com>
2025-10-31 17:48:27 +0530

    Update arconazplugin.py

--- arconazplugin.py
+++ arconazplugin.py
@@ -2,33 +2,31 @@ import sys
 from ArconSDKworking import ArconSDK
 
 
- 
-def get_sec(token,username,alias_name):
+def get_sec(token, username, alias_name):
     try:
         arcon_sdk = ArconSDK(token)
-        data = [{
-        "UserName": username,
-        "AliasName": alias_name ,
-        "OpenForHours": 10  }]
+        data = [
+            {"UserName": username, "AliasName": alias_name, "OpenForHours": 10}
+        ]
         response = arcon_sdk.get_credential(data)
         if response:
             for server_info in response:
-	            print(f"Alias: {server_info['aliasName']}")
-	            print(f"IP: {server_info['serverIp']}")
-	            print(f"User: {server_info['userName']}")	  
-				print(f"User: {server_info['password']}")	  
-				#print(f"##vso[task.setvariable variable={key};issecret=true]{server_info['password']}")
-            	#print("response:", response)
+                print(f"Alias: {server_info['aliasName']}")
+                print(f"IP: {server_info['serverIp']}")
+                print(f"User: {server_info['userName']}")
+                #print(f"Password: {server_info['password']}")
+            	print(f"##vso[task.setvariable variable={key};issecret=true]{server_info['password']}")
+            # print("response:", response)
             return response
         else:
             print("Failed to obtain response")
- 
+
     except Exception as ex:
         print("Exception occurred:", ex)
- 
- 
+
+
 if __name__ == "__main__":
     token = sys.argv[1]
     username = sys.argv[2]
     alias_name = sys.argv[3]
-    sec=get_sec(token,username,alias_name)
+    sec = get_sec(token, username, alias_name)
