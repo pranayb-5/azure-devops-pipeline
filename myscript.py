2cd19cf8cda5c723737765162c56f3573acec956
pranayb-5 <pranay.bhatia@arconnet.com>
2025-10-31 16:23:53 +0530

    Create myscript.py

---
+++ myscript.py
@@ -0,0 +1,6 @@
+# tools/myscript.py
+import sys
+# ...do work...
+result = "42"  # compute your value
+print(result)  # IMPORTANT: print only the value if you plan to capture stdout
+sys.exit(0)    # non-zero will fail the step
