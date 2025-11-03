63eed858f332222bf5ee2c14e55f5b931bc05bbb
pranayb-5 <pranay.bhatia@arconnet.com>
2025-11-03 11:54:49 +0530

    Delete myscript.py

--- myscript.py
+++
@@ -1,6 +0,0 @@
-# tools/myscript.py
-import sys
-# ...do work...
-result = "42"  # compute your value
-print(result)  # IMPORTANT: print only the value if you plan to capture stdout
-sys.exit(0)    # non-zero will fail the step
